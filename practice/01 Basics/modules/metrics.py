073b9cb4927a8271f8ad28e8e3d5f26996e3b7bf
Mswork6 <denis.ch74@mail.ru>
2026-09-15 17:40:48 +0500

    feat: made part 1 for lab1

--- practice/01 Basics/modules/metrics.py
+++ practice/01 Basics/modules/metrics.py
@@ -14,10 +14,12 @@ def ED_distance(ts1: np.ndarray, ts2: np.ndarray) -> float:
     -------
     ed_dist: euclidean distance between ts1 and ts2
     """
-    
-    ed_dist = 0
 
-    # INSERT YOUR CODE
+    # Вычисляем рызность и возводим в квадрат
+    squared_diff = (ts1 - ts2) ** 2
+
+    # Вычисляем сумму квадратов разностей и извлекаем корень
+    ed_dist = np.sqrt(np.sum(squared_diff))
 
     return ed_dist
 
@@ -58,8 +60,26 @@ def DTW_distance(ts1: np.ndarray, ts2: np.ndarray, r: float = 1) -> float:
     dtw_dist: DTW distance between ts1 and ts2
     """
 
-    dtw_dist = 0
+    n = len(ts1)
+    m = len(ts2)
 
-    # INSERT YOUR CODE
+    # Задаем матрицу, заполненную бесконечностями
+    dtw_matrix = np.full((n + 1, m + 1), np.inf)
 
-    return dtw_dist
+    # В начальной точке стоимость 0
+    dtw_matrix[0, 0] = 0
+
+    for i in range(1, n + 1):
+        for j in range(1, m + 1):
+            # Считаем квадрат разности между элементами
+            cost = (ts1[i - 1] - ts2[j - 1]) ** 2
+
+            # Берем минимум из трех возможных путей
+            dtw_matrix[i, j] = cost + min(
+                dtw_matrix[i - 1, j],       # сдвиг в первом ряду
+                dtw_matrix[i, j - 1],       # сдвиг во втором ряду
+                dtw_matrix[i - 1, j - 1]    # соответствие
+            )
+
+    # Возвращаем корень из значения в правом нижнем углу
+    return dtw_matrix[n, m]
--- practice/01 Basics/modules/pairwise_distance.py
+++ practice/01 Basics/modules/pairwise_distance.py
@@ -49,7 +49,17 @@ class PairwiseDistance:
 
         dist_func = None
 
-        # INSERT YOUR CODE
+        if self.metric == 'euclidean':
+            if self.is_normalize:
+                dist_func = norm_ED_distance
+            else:
+                dist_func = ED_distance
+        elif self.metric == 'dtw':
+
+            # Для DTW нормализация выполняется отдельно через z_normalize
+            dist_func = DTW_distance
+        else:
+            raise ValueError(f"Unsupported metric: {self.metric}")
 
         return dist_func
 
@@ -68,7 +78,17 @@ class PairwiseDistance:
         
         matrix_shape = (input_data.shape[0], input_data.shape[0])
         matrix_values = np.zeros(shape=matrix_shape)
-        
-        # INSERT YOUR CODE
+
+        # Получаем функцию расстояния
+        dist_func = self._choose_distance()
+
+        n_series = input_data.shape[0]
+
+        for i in range(n_series):
+            for j in range(i + 1, n_series):
+                distance = dist_func(input_data[i], input_data[j])
+
+                matrix_values[i, j] = distance
+                matrix_values[j, i] = distance
 
         return matrix_values
